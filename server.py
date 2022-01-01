61db5f9b8fb80f0f27e091177df41239c4354696
Russell Green <russell.green@rizing.com>
2022-01-01 14:08:03 -0500

    chroe: adds pin 12

--- server.py
+++ server.py
@@ -23,7 +23,7 @@ class InitialPinBehavior(Enum):
 host = '0.0.0.0'
 port = 8080
 debug = False
-gpio_pins = (7, 11, 13, 15, 16, 18, 22, 29, 31, 32, 33, 36, 37)
+gpio_pins = (7, 11, 12, 13, 15, 16, 18, 22, 29, 31, 32, 33, 36, 37)
 initial_pin_state = InitialPinBehavior.DEFAULT
 app = Flask(__name__)
 CORS(app)
